15bd64191a31f88451b9dafd5f1a34de68cb6a32
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-04-28 10:28:52 -0500

    Creating static methods
    Creating static methods

---
+++ ConvertTemperature.cs
@@ -0,0 +1,9 @@
+﻿class Temperature
+{
+    static FahrenheitToCelsius(var temperatureFahrenheit)
+    {
+        var temperatureCelsius = (temperatureFahrenheit - 32) / 1.8;
+        return temperatureCelsius;
+
+    }
+}
