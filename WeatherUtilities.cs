a50ba09fa4b0e57a2803836b13db5cf3bf49749c
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-05-06 14:31:38 -0500

    Cleaning up
    Cleaning up the class

--- WeatherUtilities.cs
+++ WeatherUtilities.cs
@@ -6,26 +6,25 @@ namespace MyUtilities
     {
         static float FahrenheitToCelsius(float temperatureFahrenheit)
         {
-            var temperatureCelsius = temperatureFahrenheit - 32 / 1.8f;
-            return temperatureCelsius;
+            return (float)(temperatureFahrenheit - 32 / 1.8f);
         }
 
         static float CelsiusToFahrenheit(float temperatureCelsius)
         {
-            var temperatureFahrenheit = temperatureCelsius * 1.8f + 32;
-            return temperatureFahrenheit;
+            return (float)(temperatureCelsius * 1.8f + 32);
         }
 
+        // the higher the index, the lower the confort. 
         static float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
         {
-            var comfortIndex = (temperatureFahrenheit + humidityPercent) / 4;
-            return comfortIndex;
+            // probably not a very reliable formula:
+            return (temperatureFahrenheit + humidityPercent) / 4;
         }
 
         static void Report(string location, float temperatureCelsius, float humidity)
         {
             var temperatureFahrenheit = CelsiusToFahrenheit(temperatureCelsius);
-            Console.WriteLine("Comfort Index for " + location + ": " + ComfortIndex(temperatureFahrenheit, humidity));
+            Console.WriteLine($"Comfort Index for {location}: {ComfortIndex(temperatureFahrenheit, humidity)}");
         }
     }
 }
