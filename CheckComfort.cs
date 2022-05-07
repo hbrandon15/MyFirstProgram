cd2896a53ca7928f77b3ec4014f58c57961245e7
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-05-06 22:17:28 -0500

    Accessing namespaces
    Accessing namespaces for two different programs

---
+++ CheckComfort.cs
@@ -0,0 +1,16 @@
+﻿using Weatherutilities = MyUtilities.WeatherUtilities;
+
+
+namespace MyFirstProgram
+{
+    class CheckComfort
+    {
+        static void Main(string[] args)
+        {
+            MyUtilities.WeatherUtilities.Report("Bologna", 23, 65); // ~ 73F
+
+        }
+
+
+    }
+}
