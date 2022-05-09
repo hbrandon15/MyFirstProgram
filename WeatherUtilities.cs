ab1f4b2a2327bf9125604284b37c49b305050ce2
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-05-09 11:27:35 -0500

    Small refactoring
    Small refactoring

--- WeatherUtilities.cs
+++ WeatherUtilities.cs
@@ -5,24 +5,24 @@ namespace MyUtilities
 {
     class WeatherUtilities
     {
-        static public float FahrenheitToCelsius(float temperatureFahrenheit)
+        public static float FahrenheitToCelsius(float temperatureFahrenheit)
         {
             return (float)(temperatureFahrenheit - 32) / 1.8f;
         }
 
-        static public float CelsiusToFahrenheit(float temperatureCelsius)
+        public static float CelsiusToFahrenheit(float temperatureCelsius)
         {
             return (float)(temperatureCelsius * 1.8f + 32);
         }
 
         // the higher the index, the lower the confort. 
-        static private float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
+        private static float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
         {
             // probably not a very reliable formula:
             return (temperatureFahrenheit + humidityPercent) / 4;
         }
 
-        static public void Report(string location, float temperatureCelsius, float humidity)
+        public static void Report(string location, float temperatureCelsius, float humidity)
         {
             var temperatureFahrenheit = CelsiusToFahrenheit(temperatureCelsius);
             Console.WriteLine($"Comfort Index for {location}: {ComfortIndex(temperatureFahrenheit, humidity)}");
